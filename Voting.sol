52aebb908c0e02271c5989d584598efd24aafb1d
Michael Oladejo Akanbi <110104359+oladejo-michael@users.noreply.github.com>
2023-06-02 09:10:00 +0100

    Update Voting.sol
    added real-time updates and secure vote counting

--- Voting.sol
+++ Voting.sol
@@ -1,30 +1,42 @@
 pragma solidity ^0.8.0;
 
 contract Voting {
-    mapping(address => bool) public hasVoted;  // Mapping to track whether an address has voted
-    
-    address public owner;  // Address of the contract owner
-    
-    modifier onlyOwner() {
-        require(msg.sender == owner, "Only the contract owner can perform this action.");
-        _;
-    }
-    
-    constructor() {
-        owner = msg.sender;  // Set the contract owner as the deployer
-    }
+  mapping(uint => uint) public voteCounts;
+  mapping(address => bool) public hasVoted;
+  mapping(address => bool) public isAuthorized;
 
-    function vote(uint option) public {
-        require(!hasVoted[msg.sender], "You have already voted.");  // Check if the user has already voted
-        
-        // Perform the voting logic
-        
-        hasVoted[msg.sender] = true;  // Mark the user as voted
-    }
-    
-    function resetVotes() public onlyOwner {
-        for (uint i = 0; i < voters.length; i++) {
-            hasVoted[voters[i]] = false;  // Reset the voted status for all users
-        }
-    }
+  event VoteCasted(uint option);
+
+  constructor() {
+    isAuthorized[msg.sender] = true; // Set the contract deployer as authorized by default
+  }
+
+  modifier onlyAuthorized() {
+    require(isAuthorized[msg.sender], "You are not authorized to vote.");
+    _;
+  }
+
+  function authorize(address voter) public onlyAuthorized {
+    isAuthorized[voter] = true;
+  }
+
+  function revokeAuthorization(address voter) public onlyAuthorized {
+    isAuthorized[voter] = false;
+  }
+
+  function castVote(uint option) public onlyAuthorized {
+    require(!hasVoted[msg.sender], "You have already voted.");
+
+    // Update the vote count
+    voteCounts[option]++;
+
+    // Mark the user as voted
+    hasVoted[msg.sender] = true;
+
+    emit VoteCasted(option);
+  }
+
+  function getVoteCount(uint option) public view returns (uint) {
+    return voteCounts[option];
+  }
 }
