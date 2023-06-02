db86fb3ddd0693282b5d78539397f26322fb872d
Michael Oladejo Akanbi <110104359+oladejo-michael@users.noreply.github.com>
2023-06-02 09:18:55 +0100

    Update app localdev.js
    Secure vote counting

--- app localdev.js
+++ app localdev.js
@@ -1,5 +1,7 @@
 import Web3 from 'web3';
 
+let contract;
+
 window.addEventListener('load', async () => {
   // Check if Web3 is available
   if (typeof window.ethereum !== 'undefined') {
@@ -20,16 +22,34 @@ window.addEventListener('load', async () => {
   const networkId = Object.keys(networks)[0];
   const deployedNetwork = networks[networkId];
   const contractAddress = deployedNetwork.address;
-  const contract = new web3.eth.Contract(abi, contractAddress);
+  contract = new web3.eth.Contract(abi, contractAddress);
 
   // Retrieve and display the vote counts
+  await updateVoteCounts();
+
+  // Subscribe to the VoteCasted event
+  contract.events.VoteCasted({}, async (error, event) => {
+    if (error) {
+      console.error('Error while listening to VoteCasted event:', error);
+      return;
+    }
+
+    // Update the vote counts
+    await updateVoteCounts();
+  });
+});
+
+async function updateVoteCounts() {
+  // Retrieve the vote counts
   const option1Count = await contract.methods.getVoteCount(1).call();
   const option2Count = await contract.methods.getVoteCount(2).call();
   const option3Count = await contract.methods.getVoteCount(3).call();
+
+  // Update the vote counts in the UI
   document.getElementById('option1Count').textContent = option1Count;
   document.getElementById('option2Count').textContent = option2Count;
   document.getElementById('option3Count').textContent = option3Count;
-});
+}
 
 async function castVote() {
   // Check if the user has a connected wallet
@@ -38,41 +58,21 @@ async function castVote() {
     return;
   }
 
+  // Check if the user is authorized
+  const isAuthorized = await contract.methods.isAuthorized(web3.currentProvider.selectedAddress).call();
+  if (!isAuthorized) {
+    alert('You are not authorized to vote.');
+    return;
+  }
+
   const selectedOption = document.getElementById('voteOption').value;
 
   try {
-    // Load the smart contract
-    const response = await fetch('build/contracts/Voting.json');
-    const { networks, abi } = await response.json();
-    const networkId = Object.keys(networks)[0];
-    const deployedNetwork = networks[networkId];
-    const contractAddress = deployedNetwork.address;
-    const contract = new web3.eth.Contract(abi, contractAddress);
-  
-    // Check if the user is authorized to vote
-    const isAuthorized = await contract.methods.isAuthorized(web3.currentProvider.selectedAddress).call();
-    if (!isAuthorized) {
-      alert('You are not authorized to vote.');
-      return;
-    }
-
-    // Check if the user has already voted
-    const hasVoted = await contract.methods.hasVoted(web3.currentProvider.selectedAddress).call();
-    if (hasVoted) {
-      alert('You have already voted.');
-      return;
-    }
-    
     // Cast the vote
     await contract.methods.castVote(selectedOption).send({ from: web3.currentProvider.selectedAddress });
 
     // Update the vote counts
-    const option1Count = await contract.methods.getVoteCount(1).call();
-    const option2Count = await contract.methods.getVoteCount(2).call();
-    const option3Count = await contract.methods.getVoteCount(3).call();
-    document.getElementById('option1Count').textContent = option1Count;
-    document.getElementById('option2Count').textContent = option2Count;
-    document.getElementById('option3Count').textContent = option3Count;
+    await updateVoteCounts();
 
     alert('Vote casted successfully!');
   } catch (error) {
