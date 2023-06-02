b7659d94479dc56cc1f60b58f285bb0d6bd0fe68
Michael Oladejo Akanbi <110104359+oladejo-michael@users.noreply.github.com>
2023-06-02 08:47:59 +0100

    Update Voting.sol

--- Voting.sol
+++ Voting.sol
@@ -1,22 +1,30 @@
-// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 contract Voting {
-    mapping(address => bool) public hasVoted;
-    mapping(uint256 => uint256) public voteCounts;
-
-    event VoteCasted(address indexed voter, uint256 indexed option);
-
-    function castVote(uint256 option) public {
-        require(!hasVoted[msg.sender], "Already voted");
-        
-        voteCounts[option] += 1;
-        hasVoted[msg.sender] = true;
-        
-        emit VoteCasted(msg.sender, option);
+    mapping(address => bool) public hasVoted;  // Mapping to track whether an address has voted
+    
+    address public owner;  // Address of the contract owner
+    
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only the contract owner can perform this action.");
+        _;
+    }
+    
+    constructor() {
+        owner = msg.sender;  // Set the contract owner as the deployer
     }
 
-    function getVoteCount(uint256 option) public view returns (uint256) {
-        return voteCounts[option];
+    function vote(uint option) public {
+        require(!hasVoted[msg.sender], "You have already voted.");  // Check if the user has already voted
+        
+        // Perform the voting logic
+        
+        hasVoted[msg.sender] = true;  // Mark the user as voted
+    }
+    
+    function resetVotes() public onlyOwner {
+        for (uint i = 0; i < voters.length; i++) {
+            hasVoted[voters[i]] = false;  // Reset the voted status for all users
+        }
     }
 }
